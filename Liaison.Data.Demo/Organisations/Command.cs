fb6aa7c7e4f9967a767b14b0fe722209ee762fd4
WALLYS\Gareth <surferjoeuk@hotmail.com>
2017-11-19 01:37:28 +0000

    19/11/2017

---
+++ Liaison.Data.Demo/Organisations/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liaison.Data.Demo.Organisations
+{
+    public class Command
+    {
+        public Guid CommandId { get; set; }
+        public string Name { get; set; }
+        public string Acronym { get; set; }
+        public List<string> Keywords { get; set; }
+        public Liaison.Helper.Enumerators.Services Service { get;set; }
+    }
+}
---
+++ Liaison.Helper/Enumerators/Enumerators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liaison.Helper.Enumerators
+{
+    public enum Services
+    {
+        Navy=1,
+        Army,
+        AirForce,
+        Marines,
+        Joint
+
+    }
+
+}
