bd286345fed2bf96695d0249d4d3df1ff3d824d9
LauraHPLaptop <surferjoeuk@hotmail.com>
2019-06-11 21:09:58 +0100

    test

--- Liaison.Import.RallyPoint/Program.cs
+++ Liaison.Import.RallyPoint/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Xml;
-using System.Xml.Serialization;
+using System.Xml.Serialization;x
 
 
 namespace Liaison.Import.RallyPoint
